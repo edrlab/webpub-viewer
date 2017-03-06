c69a39c97aaebb569b5e526241e976464f0c7792
Ricardo Galvez <ricardogalvez@nypl.org>
2017-03-06 01:36:47 -0500

    improved the svg icons a bit. added arrow icons to the chapter pagers

--- src/IFrameNavigator.ts
+++ src/IFrameNavigator.ts
@@ -11,23 +11,59 @@ const template = `
   <nav class="publication">
     <div class="controls">
       <ul class="links top" style="z-index: 2000;">
-        <li><a rel="start" class="disabled">Start</a></li>
-        <li><a rel="contents" class="disabled">Contents</a></li>
         <li>
-          <a class="settings" role="button" aria-label="Settings Menu" aria-expanded="false">
-              <svg viewBox="0 0 100 125" class="icon" role="img">
-                <g transform="translate(0,-952.36218)" aria-label="settings" >
-                  <title id="settings-icon">Settings</title>
-                  <path d="m 42,960.36216 -1.9062,10.5 c -1.8532,0.58249 -3.6477,1.33411 -5.3438,2.21875 l -8.8125,-6.0625 -11.3125,11.3125 6.0938,8.78125 c -0.8885,1.70097 -1.6345,3.48465 -2.2188,5.34375 l -10.4999998,1.90625 0,16.00004 L 18.5,1012.2684 c 0.5858,1.8637 1.3586,3.639 2.25,5.3438 l -6.125,8.7812 11.3125,11.3126 8.7813,-6.0626 c 1.7035,0.8904 3.5129,1.6335 5.375,2.2188 l 1.9062,10.5 16,0 1.9062,-10.5 c 1.859,-0.5844 3.6428,-1.3303 5.3438,-2.2188 l 8.7812,6.0626 11.3126,-11.3126 -6.0938,-8.7812 c 0.8847,-1.6934 1.6353,-3.4624 2.2188,-5.3125 L 92,1010.3622 92,994.36216 81.4688,992.45591 c -0.5806,-1.84671 -1.307,-3.62167 -2.1876,-5.3125 l 6.0626,-8.8125 -11.3126,-11.3125 -8.7812,6.0625 c -1.701,-0.88839 -3.4847,-1.63437 -5.3438,-2.21875 l -1.9062,-10.5 -16,0 z m 8,23 c 10.4816,0 19,8.51835 19,19.00004 0,10.4815 -8.5184,19 -19,19 -10.4815,0 -19,-8.5185 -19,-19 0,-10.4817 8.5185,-19.00004 19,-19.00004 z m 0,2 c -9.4007,0 -17,7.5992 -17,17.00004 0,9.4006 7.5993,17 17,17 9.4007,0 17,-7.5994 17,-17 0,-9.40084 -7.5993,-17.00004 -17,-17.00004 z" stroke="none"/>
-                </g>
-              </svg>
-            <span class="setting-text">Settings</span>
+          <a rel="start" class="disabled" role="button" aria-label="Book Home">
+          <svg viewBox="0 0 100 125" class="icon" role="img" aria-labledby="home-button">
+            <title id="home-button">Home Button</title>
+            <g transform="translate(0,-952.36218)">
+              <path style="text-indent:0;text-transform:none;direction:ltr;block-progression:tb;baseline-shift:baseline;color:#000000;enable-background:accumulate;" d="M 48.18752,956.9572 8.1875204,986.95679 c -0.7389,0.5602 -1.1923,1.4789 -1.1875,2.4062 l 0,55.99921 c 2e-4,1.5708 1.4292,2.9998 2.9999996,3 l 27,0 c 1.5708,-2e-4 2.9998,-1.4292 3,-3 l 0,-34.9995 20,0 0,34.9995 c 2e-4,1.5708 1.4292,2.9998 3,3 l 26.999997,0 c 1.5708,-2e-4 2.9998,-1.4292 3,-3 l 0,-55.99921 c 0,-0.9273 -0.4486,-1.846 -1.1875,-2.4062 L 51.81252,956.9572 c -1.3099,-0.8318 -2.4519,-0.75401 -3.625,0 z m 1.8125,6.15616 36.999997,27.74963 0,51.49931 -20.999997,0 0,-34.9996 c -2e-4,-1.5707 -1.4292,-2.9998 -3,-2.9999 l -26,0 c -1.5708,10e-5 -2.9998,1.4292 -3,2.9999 l 0,34.9996 -21,0 0,-51.49931 z"/>
+            </g>
+          </svg>
+            <span class="setting-text home">Start</span>
+          </a>
+        </li>
+        <li>
+          <a rel="contents" class="disabled" role="button" aria-label="Book Content" aria-haspopup="true" aria-expanded="false">
+            <svg viewBox="0 0 24 24" class="icon" role="img" aria-label="contents">
+              <title id="content-icon">Contents</title>
+              <path d="M20.5,0h-17C2.673,0,2,0.673,2,1.5v16c0,0.065,0.013,0.13,0.039,0.191c0.025,0.06,0.061,0.113,0.105,0.158  c0.001,0.001,0.001,0.003,0.002,0.004l6,6c0.001,0.002,0.004,0.002,0.006,0.004c0.045,0.044,0.098,0.08,0.157,0.104  C8.37,23.987,8.435,24,8.5,24h12c0.827,0,1.5-0.673,1.5-1.5v-21C22,0.673,21.327,0,20.5,0z M8,22.293L3.707,18H7.5  C7.776,18,8,18.224,8,18.5V22.293z M21,22.5c0,0.276-0.224,0.5-0.5,0.5H9v-4.5C9,17.673,8.327,17,7.5,17H3V1.5  C3,1.224,3.224,1,3.5,1h17C20.776,1,21,1.224,21,1.5V22.5z"></path>
+              <path d="M17,5H7C6.724,5,6.5,4.776,6.5,4.5S6.724,4,7,4h10c0.276,0,0.5,0.224,0.5,0.5S17.276,5,17,5z"></path>
+              <path d="M17,8H7C6.724,8,6.5,7.776,6.5,7.5S6.724,7,7,7h10c0.276,0,0.5,0.224,0.5,0.5S17.276,8,17,8z"></path>
+              <path d="M12,11H7c-0.276,0-0.5-0.224-0.5-0.5S6.724,10,7,10h5c0.276,0,0.5,0.224,0.5,0.5S12.276,11,12,11z"></path>
+            </svg>
+            <span class="setting-text contents">Contents</span>
+          </a>
+        </li>
+        <li>
+          <a class="settings" role="button" aria-label="Settings Menu" aria-expanded="false" aria-haspopup="true">
+            <svg class="icon" viewBox="0 0 86 86" aria-label="settings" role="img">
+              <title id="settings-icon">setting-icon</title>
+              <path d="M51.86,86H34.19L32.24,75.31a34,34,0,0,1-4.42-1.82l-9,6.2L6.35,67.19l6.26-9a33.46,33.46,0,0,1-1.85-4.39L0,51.88V34.21l10.73-1.95a33.87,33.87,0,0,1,1.82-4.39l-6.23-9L18.84,6.4l9,6.2a34,34,0,0,1,4.39-1.82L34.19,0H51.86l1.95,10.73A33.84,33.84,0,0,1,58.2,12.6l9-6.2L79.66,18.89l-6.2,9a33.74,33.74,0,0,1,1.79,4.36L86,34.2V51.87l-10.76,2a33.89,33.89,0,0,1-1.82,4.36l6.23,9L67.17,79.68l-9-6.2a33.7,33.7,0,0,1-4.39,1.82Zm-16-2H50.19l1.87-10.27.58-.18a31.81,31.81,0,0,0,5.18-2.15l.53-.28,8.59,5.93L77.07,67l-6-8.59.28-.54a31.8,31.8,0,0,0,2.15-5.15l.18-.57L84,50.21V35.88L73.72,34l-.18-.58a31.84,31.84,0,0,0-2.12-5.15l-.28-.53,5.93-8.62L66.94,9l-8.59,5.93-.54-.28a31.85,31.85,0,0,0-5.18-2.15l-.58-.18L50.19,2H35.86L34,12.31l-.58.18a32,32,0,0,0-5.18,2.15l-.53.28L19.08,9,8.95,19.13l6,8.59-.28.54a31.87,31.87,0,0,0-2.15,5.18L12.3,34,2,35.88V50.21L12.3,52.07l.18.58a31.49,31.49,0,0,0,2.18,5.18l.28.54L8.95,67,19.08,77.09l8.59-5.93.53.28a32,32,0,0,0,5.21,2.15l.58.18Z" transform="translate(-0.02 -0.04)"/>
+              <path d="M43,63A20,20,0,1,1,63,43,20,20,0,0,1,43,63Zm0-38A18,18,0,1,0,61,43,18,18,0,0,0,43,25Z" transform="translate(-0.02 -0.04)"/>
+            </svg>
+            <span class="setting-text settings">Settings</span>
             </a>
         </li>
       </ul>
       <ul class="links bottom" style="z-index: 2000;">
-        <li><a rel="prev" class="disabled">Previous Chapter</a></li>
-        <li><a rel="next" class="disabled">Next Chapter</a></li>
+        <li>
+        <a rel="prev" class="disabled">
+          <svg width="48" height="48" viewBox="0 0 32 32" class="icon">
+            <title>wedge.left.icon.svg</title>
+            <polygon points="19.156 23.563 11.593 16 19.156 8.438 20.563 9.845 14.408 16 20.563 22.155 19.156 23.563" />
+          </svg>
+          <span class="chapter-control">Previous Chapter</span>
+          </a>
+        </li>
+        <li>
+          <a rel="next" class="disabled">
+            <span class="chapter-control">Next Chapter</span>
+            <svg width="48" height="48" viewBox="0 0 32 32" class="icon">
+            <title>wedge.right.icon.svg</title>
+            <polygon points="13 8.437 20.563 16 13 23.563 11.593 22.155 17.748 16 11.593 9.845 13 8.437"/>
+            </svg>
+          </a>
+        </li>
       </ul>
       <div class="pagination-controls" style="-webkit-tap-highlight-color: transparent; display: none;">
         <div class="previous-page" style="position: fixed; top: 0; left: 0; width: 30%; height: 100%; z-index: 1000;"></div>
