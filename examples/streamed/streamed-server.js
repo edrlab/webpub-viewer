17bde37a3ed0f09d30dd81d4075864f2cc05b56e
Juan Corona <juan.carlos.corona.r@gmail.com>
2019-04-01 16:23:15 -0700

    Use streamer from npm, instead of local copy

---
+++ examples/streamed/streamed-server.js
@@ -0,0 +1,4 @@
+//TODO: needed until this is resolved: https://github.com/readium/r2-streamer-js/issues/44
+require("r2-streamer-js/dist/es5/src/http/server-cli")
+
+console.log('Streamer server started.');
